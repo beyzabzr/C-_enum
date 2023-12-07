44edbc62691e63a33da8de93ed8d7cd5be5ad1fb
beyzabzr <113631271+beyzabzr@users.noreply.github.com>
2023-12-07 21:24:03 +0300

    main

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace _enum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***ENUM GUNLER***");
+
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            Console.WriteLine("***ENUM HAVA DURUMU***");
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Disariya cikmak icin havanin birazdaha isinmasini bekleyelim");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Disariya cikmak icin cok sicak bir gun");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hadi disariya cikalim");
+            }
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
